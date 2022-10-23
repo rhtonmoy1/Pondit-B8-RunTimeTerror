b0882cb98db3e3724e1f632f40103cd7bc7a18aa
MD ABDUL MANANN <111185314+mannan-web-dev@users.noreply.github.com>
2022-10-23 11:58:47 +0600

    Update web.php
    admin sidebar view file change...with controller

--- e-com/routes/web.php
+++ e-com/routes/web.php
@@ -1,6 +1,13 @@
 <?php
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ColorController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 
 use Illuminate\Support\Facades\Route;
+
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +20,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('admin')->name('admin.')->group(function () {
+// Route::prefix('admin')->name('admin.')->group(function () {
 
-    Route::get('/dashboard', function () { return view('admin.dashboard'); })->name('dashboard');
-    Route::get('/account', function () { return view('admin.account'); })->name('account');
-    Route::get('/product', function () { return view('admin.product'); })->name('product');
+//     Route::get('/dashboard', function () { return view('admin.dashboard'); })->name('dashboard');
+//     Route::get('/account', function () { return view('admin.account'); })->name('account');
+//     Route::get('/product', function () { return view('admin.product'); })->name('product');
+// });
+
+
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+
+Route::get('/admin/dashboard', [DashboardController::class,'index'])->name('dashboard.index');
+
+
+// Admin Account Section
+Route::get('/dashboard/account', [AccountController::class,'index'])->name('account.index');
+
+
+// Admin Product Section
+Route::get('/dashboard/products', [ProductController::class,'index'])->name('products.index');
+Route::get('/dashboard/products/create', [ProductController::class,'create'])->name('products.create');
+Route::get('/dashboard/products/trash', [ProductController::class,'trash'])->name('products.trash');
+
+
+// Admin Category Section
+Route::get('/dashboard/categories', [CategoryController::class,'index'])->name('categories.index');
+Route::get('/dashboard/categories/create', [CategoryController::class,'create'])->name('categories.create');
+Route::get('/dashboard/categories/trash', [CategoryController::class,'trash'])->name('categories.trash');
+
+// Admin Color Section
+Route::get('/dashboard/colors', [ColorController::class,'index'])->name('colors.index');
+Route::get('/dashboard/colors/create', [ColorController::class,'create'])->name('colors.create');
+Route::get('/dashboard/colors/trash', [ColorController::class,'trash'])->name('colors.trash');
 
 
 
