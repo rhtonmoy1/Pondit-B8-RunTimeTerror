53967fa57cdf2a3c3e221bc8206c625c10db6dff
ai-ashraf <arifulislam161148@gmail.com>
2022-10-23 16:07:18 +0600

    Link change in header file

--- e-com/resources/views/components/frontend/partials/header.blade.php
+++ e-com/resources/views/components/frontend/partials/header.blade.php
@@ -58,20 +58,20 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ">
                 <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="about."> About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="product.html">Products</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="why.html">Why Us</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="testimonial.html">Testimonial</a>
-                </li>
+                    <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}"> About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('product') }}">Products</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('why') }}">Why Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('testimonial') }}">Testimonial</a>
+                  </li>
               </ul>
             </div>
           </nav>
