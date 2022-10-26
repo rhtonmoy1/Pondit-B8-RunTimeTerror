cd5d9207e9598df9ee4b669f20a595d9cd4dcc0f
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2022-10-26 13:50:54 +0600

    Revert "Final Project"
    This reverts commit aa0255090385913b95640248695bfb9039965e8e.

--- Final Project/storage/framework/views/124a6a04d454da4cf2fc5be87bc2d05ecbc61790.php
+++
@@ -1,9 +0,0 @@
-<?php if($errors->any()): ?>
-<div class="alert alert-danger">
-    <ul>
-        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <li><?php echo e($error); ?></li>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-    </ul>
-</div>
-<?php endif; ?><?php /**PATH C:\Software\wamp64\www\seip-laravel-b8-main\resources\views/components/forms/errors.blade.php ENDPATH**/ ?>
--- Final Project/storage/framework/views/ce661553cd63ead0411b9629ee5b2b19094438b8.php
+++
@@ -1,6 +0,0 @@
-<?php if(session('message')): ?>
-<p class="text-success">
-    <?php echo e(session('message')); ?>
-
-</p>
-<?php endif; ?><?php /**PATH C:\Software\wamp64\www\seip-laravel-b8-main\resources\views/components/forms/message.blade.php ENDPATH**/ ?>
